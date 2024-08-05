cf649a09bd5d0ed27f1137d9e5342da2178cb345
Marco A Guzman <marcoguzman16@gmail.com>
2024-08-05 16:04:02 -0700

    primes, for loops

--- Javascript/JS_Fundamentals/While_For_Loops/main.js
+++ Javascript/JS_Fundamentals/While_For_Loops/main.js
@@ -164,3 +164,100 @@ Hence the comparison is actually while(4 < 5) – true, and the control goes on 
 
 The value i = 5 is the last one, because on the next step while(5 < 5) is false.
 */
+
+// Prefix and Postfix form in FOR loops
+for (let i = 0; i < 5; i++) alert( i );
+
+for (let i = 0; i < 5; ++i) alert( i );
+
+/*
+You get 0 to 4 in both cases
+
+That can be easily deducted from the algorithm of for:
+
+Execute once i = 0 before everything (begin).
+Check the condition i < 5
+If true – execute the loop body alert(i), and then i++
+The increment i++ is separated from the condition check (2). That’s just another statement.
+
+The value returned by the increment is not used here, so there’s no difference between i++ and ++i.
+*/
+
+/* 
+Output Even numbers in the Loop
+
+Use the for loop to output even numbers from 2 to 10.
+*/
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log( i );
+  }
+}
+
+/* Replace "for" with "while" loop
+
+Rewrite the code changing the for loop to while without altering
+its behavior (the output should stay same).
+
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+}
+  */
+
+let i = 0;
+while (i < 3){
+    alert(`number ${i}!`)
+    i++;
+}
+
+/* 
+Repeat until input is correct
+
+Write a loop which prompts for a number greater than 100. 
+If the visitor enters another number – ask them to input again.
+
+The loop must ask for a number until either the visitor enters a number greater than 100 
+or cancels the input/enters an empty line.
+
+Here we can assume that the visitor only inputs numbers. There’s no need to implement a 
+special handling for a non-numeric input in this task.
+
+*/
+
+let userInput = Number(prompt("Please enter a number greater than 100", 0));
+
+if(userInput <= 100){
+ console.log("Please try again bro!!! ");
+}
+
+/*
+
+Output Prime Numbers
+
+An integer number greater than 1 is called a prime if it cannot be divided without a remainder by anything except 1 and itself.
+
+In other words, n > 1 is a prime if it can’t be evenly divided by anything except 1 and n.
+
+For example, 5 is a prime, because it cannot be divided without a remainder by 2, 3 and 4.
+
+Write the code which outputs prime numbers in the interval from 2 to n.
+
+For n = 10 the result will be 2,3,5,7.
+
+P.S. The code should work for any n, not be hard-tuned for any fixed value.
+
+
+*/
+let n = 10;
+
+//use a label
+nextPrime:
+for (let i = 2; i <= n; i++) { // for each i...
+
+  for (let j = 2; j < i; j++) { // look for a divisor..
+    if (i % j == 0) continue nextPrime; // not a prime, go next i
+  }
+
+  console.log( i ); // a prime
+}
