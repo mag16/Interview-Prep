c3d86f7e9c219de2053db03635fe80828c6eff4d
Marco A Guzman <marcoguzman16@gmail.com>
2024-08-19 13:26:14 -0700

    JS String methods

---
+++ Javascript/JS_Methods/String_Methods/main.js
@@ -0,0 +1,31 @@
+// JavaScript String Methods
+
+const str = "Hello World";
+
+str.length; // 11
+
+str.charAt(4); // o
+
+str.endsWith("d"); // true
+
+str.includes("World"); // true
+
+str.indexOf("W"); // 6
+
+str.repeat(2);// "Hellow WorldHello World";
+
+str.replace("World", "Marco"); // Hello Marco;
+
+str.slice(6, 10); // "Worl"
+
+str.split(" "); // ["Hello World"]
+
+str.startsWith("Hello");
+
+str.substring(6, 11); // "World";
+
+str.toLowerCase(); // "hello world"
+
+str.toUpperCase(); // "HELLO WORLD"
+
+"   Hello World    ".trim(); // "Hello World"
