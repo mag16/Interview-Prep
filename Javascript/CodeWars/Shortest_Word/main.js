099882fc254a46d34a8e0d21e953e6ea0adb417f
Marco A Guzman <marcoguzman16@gmail.com>
2024-09-02 14:43:33 -0700

    formatting

--- Javascript/CodeWars/Shortest_Word/main.js
+++ Javascript/CodeWars/Shortest_Word/main.js
@@ -8,6 +8,17 @@ String will never be empty and you do not need to account for different data typ
 */
 
 function findShort(s){
-  
-  
-}
+    let words = s.split(" ");
+    let shortestWord = words[0];
+    
+    for (const word of words) {
+      if (word.length < shortestWord.length) {
+        shortestWord = word;
+      }  
+    } 
+    
+    return shortestWord.length;
+    
+  }
+
+  console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
