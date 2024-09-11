27c59de32a093dd7ac943e27132cb995463db5bd
Marco A Guzman <marcoguzman16@gmail.com>
2024-09-11 13:34:20 -0700

    Higher Order functions

--- Javascript/JS_Fundamentals/JS_The_Hard_Parts/main.js
+++ Javascript/JS_Fundamentals/JS_The_Hard_Parts/main.js
@@ -6,7 +6,7 @@ Goes through the code
 line by line and runs/executes each line
 known as the thread of execution.
 
-Save 'data'like strings and arrays so we can
+Save 'data' like strings and arrays so we can
 use the data later - in its memory
 
 We can save code ('Functions')
@@ -21,3 +21,30 @@ function multiplyBy2 (inputNumber) {
 
 const output = multiplyBy2(num);
 const newOutput = multiplyBy2(10);
+
+
+//functions are used for reusable code
+function numSquared() {
+    return num * num;
+}
+
+
+
+//functions are first class objects
+/* 
+they can co-exist with and can be treated like any other JS Object
+1. Assigned to variables and properties of other objects
+2. Passed as arguments into functions
+3. Returned as values from functions
+
+*/
+function copyArrayAndManipulate(array, instructions) {
+    const output = [];
+    for (let i = 0; i < array.length; i++) {
+        output.push(instructions(array[i]))
+    }
+    return output;
+}
+
+function multiplyBy2(input) { return input * 2;}
+const result = copyArrayAndManipulate([1, 2, 3], multiplyBy2);
