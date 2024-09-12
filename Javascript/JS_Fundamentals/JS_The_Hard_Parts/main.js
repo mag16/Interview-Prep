c7f32b6eb8562054a4f138a257b5cafe29b06b15
Marco A Guzman <marcoguzman16@gmail.com>
2024-09-12 10:11:45 -0700

    closure technical definition

--- Javascript/JS_Fundamentals/JS_The_Hard_Parts/main.js
+++ Javascript/JS_Fundamentals/JS_The_Hard_Parts/main.js
@@ -38,6 +38,7 @@ they can co-exist with and can be treated like any other JS Object
 3. Returned as values from functions
 
 */
+// higher order function
 function copyArrayAndManipulate(array, instructions) {
     const output = [];
     for (let i = 0; i < array.length; i++) {
@@ -46,5 +47,68 @@ function copyArrayAndManipulate(array, instructions) {
     return output;
 }
 
-function multiplyBy2(input) { return input * 2;}
+function multiplyBy2(input) { return input * 2;} // the function we insert is the callback function
 const result = copyArrayAndManipulate([1, 2, 3], multiplyBy2);
+
+// Arrow functions
+const multiplyBy2 = input => input * 2;
+
+/*
+FUNCTIONS WITH MEMORIES
+* When our functions get called, we create a live storage of 
+data (local memory/variable environment/state) for that functions
+execution context.
+
+* When the function finishes executing, its local memory is deleted (except the returned value)
+
+* But what if our functions could hold on to live data between executions?
+
+* This would let our function definitions have an associated cache/persistent memory
+
+* But it all starts with us returning a function from another function.
+
+*/
+
+/*
+Functions can be returned from other functions in JavaScript
+*/
+
+function createFunction() {
+    function multiplyBy2(num) {
+        return num*2;
+    }
+}
+
+const generatedFunc = createFunction();
+const results = generatedFunc(3); // 6
+
+
+
+// Calling a function in the same function as it was defined
+
+function outer() {
+    let counter = 0;
+    //Closure
+    function incrementCounter() {
+        counter++;
+    }
+    incrementCounter();
+}
+const myNewFunction = outer();  
+myNewFunction();
+myNewFunction();
+
+// Where you define your functions determines what data is has access to when you call it.
+
+/*
+What can we call this "Backpack"?
+
+- Closed over "Variable Environment" (C.O.V.E)
+- Persistent Lexical Scope Referenced Data (P.L.S.R.D)
+- "Backpack"
+- "Closure"
+
+The backpack (or closure) of live data is attached incrementCounter (thenm to myNewFunction)
+thorough a hidden property known as [[scope]] which persists when the inner function is returned out.
+
+*/
