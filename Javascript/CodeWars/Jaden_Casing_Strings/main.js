62779e171552ad275e2815f64d84e631c3b941b4
Marco A Guzman <marcoguzman16@gmail.com>
2024-09-17 16:19:47 -0700

    .substring deprecated.  used slice()

--- Javascript/CodeWars/Jaden_Casing_Strings/main.js
+++ Javascript/CodeWars/Jaden_Casing_Strings/main.js
@@ -15,7 +15,7 @@ String.prototype.toJadenCase = function() {
     let words = this.split(" ");
   
     for (let i = 0; i < words.length; i++){  
-        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
     }
     return words.join(" ");
 };
