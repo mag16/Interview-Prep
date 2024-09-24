9077b810214916ec7a062578ec4d2bdbbe75bbb4
Marco A Guzman <marcoguzman16@gmail.com>
2024-09-24 12:20:16 -0700

    recursion notes

--- Javascript/JS_Fundamentals/Recursion/recursion.js
+++ Javascript/JS_Fundamentals/Recursion/recursion.js
@@ -43,3 +43,50 @@ function computeFactorial(num) {
 }
 
 console.log(computeFactorial());
+
+// Task: rewrite this function so that it uses a loop rather than recursion
+
+function joinElements(array, joinString) {
+    let resultSoFar = "";
+
+    for (let i = 0; i <= array.length - 1; i++){
+        resultSoFar += array[i] + joinString;
+    }
+
+    return resultSoFar + array[array.length - 1];
+}
+
+console.log(joinElements(['s','cr', 't cod', ' :) :)'], 'e'));
+
+
+// memoized factorial function
+const memoize = (fn) => {
+    let cache = {};
+    return (...args) => {
+        let n = args[0];
+        if (n in cache) {
+            console.log('Fetching from cache',n);
+            return cache[n];
+        }
+        else {
+            console.log('Calculating result', n);
+            let result = fn(n); //callback
+            cache[n] = result;
+            return result;
+        }
+    }
+}
+
+const factorial = memoize (
+    (x) => {
+        if (x === 0) {
+            return 1;
+        }
+        else {
+            return x * factorial(x - 1);
+        }
+    }
+);
+
+console.log(factorial(5)); // calculated
+console.log(factorial(6)); // calculated for 6 and cached for 5.
