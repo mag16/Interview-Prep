9109c41ffd6caa8e4b6f026deb585a0ef0310687
Marco A Guzman <marcoguzman16@gmail.com>
2024-09-24 10:57:54 -0700

    recursion notes

--- Javascript/JS_Fundamentals/Recursion/recursion.js
+++ Javascript/JS_Fundamentals/Recursion/recursion.js
@@ -1,1 +1,45 @@
 //A practical guide to Algorithms with Javascript
+
+/* Recursion in 4 Steps
+
+1. Identify base cases.
+2. Identify recursive cases
+3. Return where appropriate
+4. Write procedures for each
+case that brings you closer to the base case(s)
+
+*/
+
+let callMyself = function() {
+    if("case") {
+        //base case
+        return;
+    } else {
+        //recursive case
+        callMyself();
+    }
+
+    return;
+};
+
+const loopNTimes = (n) => {
+    console.log('n ===', n);
+    if (n <= 1) {
+        return 'complete'
+    }
+    return loopNTimes(n-1);
+}
+
+
+function computeFactorial(num) {
+    let result = 1;
+
+    for (let i = 2; i <= num; i++) {
+        console.log(`result = ${result} * ${i} (${result * i})`);
+        result *= i;
+    }
+
+    return result;
+}
+
+console.log(computeFactorial());
