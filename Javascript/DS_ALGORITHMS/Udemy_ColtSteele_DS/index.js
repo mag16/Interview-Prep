07ce5e158fc03b90af42eede23d492a85f3a8dba
Marco A Guzman <marcoguzman16@gmail.com>
2024-10-25 15:29:17 -0700

    sumZero w 2 pointers

--- Javascript/DS_ALGORITHMS/Udemy_ColtSteele_DS/index.js
+++ Javascript/DS_ALGORITHMS/Udemy_ColtSteele_DS/index.js
@@ -295,17 +295,34 @@ very efficient for solving problems with a minimal space complexity as well
 
 /*
 SumZero
-write a function called sumZero which accepts a sorted arrayh of integers. 
+write a function called sumZero which accepts a sorted array of integers. 
 The function should find the first pair where the sum is zero.  Return an array that includes
 both values that sum to zero on undefined if a pair does not exist.
+
+time 0(N)
+space 0(1)
 */
 
 const sumZero = (arr) => {
-
-
-
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    
+    return undefined; // If no pairs found
 }
 
 console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])) // [-3, -3]
 console.log(sumZero([-2, 0, 1, 3])) // undefined
 console.log(sumZero([1, 2, 3])) //undefined
+console.log(sumZero([-5, 1, 2, 3, 4, 5])) // [-5, 5]
